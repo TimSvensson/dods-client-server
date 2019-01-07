96dfd5e183bea781e257939e689f2f9410d76cd8
Tim Svensson <svensson_tim@hotmail.se>
2019-01-07 16:54:08 +0100

    A days work

--- client.py
+++ client.py
@@ -1,21 +1,56 @@
+import socket
+import server
+import sys
+from multiprocessing import Process
+import util
+
 class client():
 	"""
 	"""
 
-	def __init__(self):
-		pass
+	def __init__(self, ip='localhost', port=0):
+		self.ip = ip
+		self.port = port
 
 	def connect(self):
-		pass
+		self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.sock.connect((self.ip, self.port))
 
-	def disconnect(self):
-		pass
+	def close(self):
+		self.sock.close()
+		self.sock = None
 
-	def send(self):
-		pass
+	def send(self, message, recipients=None):
+		"""
+		Sends the 'message' to all in clinets in 'recipients'.
 
+		If 'recipients' is either an empty list or None, send 'message' to all clients.
+		"""
+
+		if recipients == None:
+			util.send_msg(self.sock, message)
+		
 	def recv(self):
-		pass
+		try:
+			msg = util.recv_msg(self.sock)
+		except ConnectionAbortedError as e:
+			print("Connection aborted, reconnecting...")
+			self.connect()
+			return b''
+		return msg
 
 	def info(self):
 		pass
+
+if __name__ == '__main__':
+	HOST, PORT = 'localhost', 9999
+	c = client(HOST, PORT)
+	user = ''
+	c.connect()
+
+	while user != "end":
+		user = input("Send to server > ")
+		c.send(bytes(user, 'utf-8'))
+		print("Reply: {}".format(str(c.recv(), 'utf-8')))
+
+	c.close()
--- server.py
+++ server.py
@@ -1,15 +1,44 @@
-class server():
+import socketserver
+import sys
+import util
+
+"""
+TODO:
+ * Logging
+ * Backup Server
+ * Testing
+ * 
+"""
+
+class TCPHandler(socketserver.BaseRequestHandler):
 	"""
-	"""
+    The RequestHandler class for our server.
 
-	def __init__(self):
-		pass
+    It is instantiated once per connection to the server, and must
+    override the handle() method to implement communication to the
+    client.
+    """
 
-	def start(self):
-		pass
+	def handle(self):
+		self.data = b''
+		while self.data != b'end':
+			self.data = util.recv_msg(self.request)
+			print("{}:{} wrote:".format(self.client_address[0], self.client_address[1]))
+			print(self.data)
+			util.send_msg(self.request, self.data.upper())
 
-	def stop(self):
-		pass
+def serverRun():
+	HOST, PORT = "localhost", 9999
+	if len(sys.argv) == 2:
+		PORT = int(sys.argv[1])
+	elif len(sys.argv) == 3:
+		HOST = sys.argv[1]
+		PORT = int(sys.argv[2])
+	print("HOST {}, PORT {}".format(HOST, PORT))
 
-	def info(self):
-		pass
+	with socketserver.TCPServer((HOST, PORT), TCPHandler) as server:
+		print("Server running on {}:{}".format(server.server_address[0], server.server_address[1]))
+		server.serve_forever()
+
+if __name__ == "__main__":
+	serverRun()
